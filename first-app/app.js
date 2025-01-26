9f3c5076c15744eca746f9037a1e3f7fbfd29717
AfshalG <afshalkingmaker@gmail.com>
2025-01-26 13:13:35 +0800

    Initial commit with all Node subfolders

---
+++ first-app/app.js
@@ -0,0 +1,4 @@
+const path = require('path');
+
+let obj = path.parse(__filename);
+console.log(obj);
---
+++ node-app/index.js
@@ -0,0 +1,3 @@
+const lion = require('afshalg-lion-lib');
+let result = lion.add(3,9);
+console.log(result);
